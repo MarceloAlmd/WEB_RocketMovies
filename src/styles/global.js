af097a41cd7e2a305f8dfd171ff18810c2e48415
Marcelo Magalhaes <marcelo3x4@hotmail.com>
2023-09-08 09:55:38 -0300

    created theme and global styles

---
+++ src/styles/global.js
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    font-size: 1rem;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button, textarea {
+    font-family: 'Roboto Slab', serif;
+
+  }
+  
+  a {
+    text-align: none;
+  }
+
+  button, a {
+    cursor: pointer;
+    transition: filter 0.6s;
+  }
+
+  button:hover, a:hover {
+    filter: brightness(9);
+  }
+
+`;
---
+++ src/styles/theme.js
@@ -0,0 +1,16 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "#0D0C0F",
+    BACKGROUND_800: "#1C1B1E",
+    BACKGROUND_700: "#262529",
+
+    PINK_500: "#FF859B",
+
+    BROWN_100: "#272024",
+
+    GRAY_100: "#948F99",
+    GRAY_300: "#312E38",
+
+    WHITE: "#F4EDE8",
+  },
+};

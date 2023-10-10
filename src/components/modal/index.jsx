5de80783675b1cd4cd16d828fb91bc49668f97e9
Marcelo Magalhaes <marcelo3x4@hotmail.com>
2023-10-10 17:28:43 -0300

    feat: Modal comp

---
+++ src/components/modal/index.jsx
@@ -0,0 +1,21 @@
+import * as Styles from "./styles";
+
+export function Modal({ message, cancel, confirm, onCLose }) {
+  return (
+    <Styles.Container onClick={onCLose}>
+      <Styles.Content>
+        <h1>Confirme a ação</h1>
+        <h4>{message}</h4>
+
+        <div className="contentButton">
+          <button className="buttonCancel" onClick={cancel}>
+            CANCELAR
+          </button>
+          <button className="buttonConfirm" onClick={confirm}>
+            CONFIRMAR
+          </button>
+        </div>
+      </Styles.Content>
+    </Styles.Container>
+  );
+}

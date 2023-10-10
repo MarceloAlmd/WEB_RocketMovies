4d18c0573cc73e5b1f78c898e6d4019868c13d37
Marcelo Magalhaes <marcelo3x4@hotmail.com>
2023-10-10 15:55:10 -0300

    feat: comp for error form

---
+++ src/components/formError/index.jsx
@@ -0,0 +1,5 @@
+import * as Styles from "./styles";
+
+export function FormError({ messageError }) {
+  return <Styles.Container>{messageError}</Styles.Container>;
+}
---
+++ src/components/formError/styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Container = styled.span`
+  color: ${({ theme }) => theme.COLORS.RED};
+  margin: -0.5rem 0 0.5rem 0;
+`;
